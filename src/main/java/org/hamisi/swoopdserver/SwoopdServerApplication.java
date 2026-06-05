237724b163ad56e6f2307cf39b7d2f31fbf778a2
soipanhamisi <soipanhani@gmail.com>
2026-06-05 13:25:07 +0300

    first commit

---
+++ src/main/java/org/hamisi/swoopdserver/SwoopdServerApplication.java
@@ -0,0 +1,13 @@
+package org.hamisi.swoopdserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwoopdServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwoopdServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hamisi/swoopdserver/SwoopdServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hamisi.swoopdserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwoopdServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
