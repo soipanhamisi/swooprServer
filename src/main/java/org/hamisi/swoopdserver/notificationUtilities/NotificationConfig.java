3e457bebc04cb12de60ed6a0d37860ca225e3906
soipanhamisi <soipanhani@gmail.com>
2026-08-04 17:19:28 +0300

    fixing

--- src/main/java/org/hamisi/swoopdserver/notificationUtilities/NotificationConfig.java
+++ src/main/java/org/hamisi/swoopdserver/notificationUtilities/NotificationConfig.java
@@ -1,5 +1,7 @@
 package org.hamisi.swoopdserver.notificationUtilities;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +15,9 @@ public class NotificationConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
